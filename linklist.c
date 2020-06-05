c8e37b8e9fc6d6659f7bdb4ca68e469850685864
Sahil Pant <sahilpant16@hotmail.com>
2020-06-05 11:48:07 +0530

    second commit

--- linklist.c
+++ linklist.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct listNode
 {
     int data;
@@ -6,24 +7,86 @@ struct listNode
 }**head=NULL;
 
 
-void insert(int data,int position)
+void createList(){
+
+}
+
+void insertEnd(int data){
+
+}
+
+void insertRandom(){
+
+}
+
+void deleteBeg(){
+
+}
+
+void deleteEnd(){
+
+}
+
+void deleteRandom(){
+
+}
+
+void insertBeg(int data)
 {
-    int k=1;
-    struct listNode *p,*newNode,*q;
+    struct listNode *p,*newNode;
     newNode = (struct listNode*)malloc(sizeof(struct listNode));
-    if(!newNode)
+    p = *head;
+     
+}
+
+void displayList(){
+
+}
+
+void displayReverse(){
+
+}
+
+
+void main()
+{
+    char ch;int choice,data;
+    printf("Do You want to create a Link List?(Y/N)::");
+    scanf("%c",&ch);
+    (ch=='y'||ch=='Y') ? createList() : exit(0);
+    while(1)
     {
-        printf("No memmory left");
-        return;
+        printf("choose from the following options::");
+        printf("1.Insert at the end::");
+        printf("2.Insert at the Beginning::");
+        printf("3.Insert at a random position::");
+        printf("4.delete at the end::");
+        printf("5.delete at the beginning::");
+        printf("5.delete at any random position::");
+        printf("6.Display the list::");
+        printf("7.Display the list in reverse order::");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            printf("Enter the Data::");
+            scanf("%d",&data);
+            insertEnd(data);
+            
+            case 2:
+            printf("Enter the Data::");
+            scanf("%d",&data);
+            insertBeg(data);
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+        }
     }
-    newNode->data = data;
-    if(position==1)
-    {
-        head = newNode;
-        p = *head;
-    }
-    else
-    {
-        while()
-    }   
 }
